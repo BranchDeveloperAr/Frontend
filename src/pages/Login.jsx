500db4e05c94b877d5dc5996d6d615c4e99053ed
BranchDeveloperAr <branchdevelopment.ar@gmail.com>
2025-10-27 20:52:51 -0300

    .

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -19,7 +19,7 @@ export default function Login({ setUser }) {
     try {
       const deviceId = 'web-' + window.navigator.userAgent
 
-      const response = await fetch('https://backend-iota-sand-32.vercel.app/api/auth/login-web', {
+      const response = await fetch('http://localhost:3000/api/auth/login-web', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
